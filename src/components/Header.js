04d0e44d9f4faed7b54a3aca7fb62428383491ee
Oliver Weber <oliver@mondough.io>
2020-03-19 19:02:53 +0100

    Modal for login and signup

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Box, Heading, Flex, Text, Button, ButtonGroup } from "@chakra-ui/core";
+import {
+  Modal,
+  ModalBody,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalCloseButton,
+  ModalFooter,
+  FormControl,
+  FormLabel,
+  Input,
+  useDisclosure
+} from "@chakra-ui/core";
 
 const MenuItems = ({ children, to }) => (
   <Link to={to}>
@@ -11,8 +24,19 @@ const MenuItems = ({ children, to }) => (
 );
 
 const Header = (props) => {
-  const [show, setShow] = React.useState(false);
-  const handleToggle = () => setShow(!show);
+  // Menu
+  const [showMenu, setMenu] = React.useState(false);
+  const handleMenu = () => setMenu(!showMenu);
+
+  // Modal Control
+  const {
+    isOpen: formIsOpen,
+    onOpen: formOnOpen,
+    onClose: formOnClose
+  } = useDisclosure();
+
+  // Signup or Login
+  const [isSignup, setSignup] = React.useState(false);
 
   return (
     <Flex
@@ -31,7 +55,7 @@ const Header = (props) => {
         </Heading>
       </Flex>
 
-      <Box display={{ base: "block", md: "none" }} onClick={handleToggle}>
+      <Box display={{ base: "block", md: "none" }} onClick={handleMenu}>
         <svg
           fill="white"
           width="12px"
@@ -44,7 +68,7 @@ const Header = (props) => {
       </Box>
 
       <Box
-        display={{ base: show ? "block" : "none", md: "flex" }}
+        display={{ base: showMenu ? "block" : "none", md: "flex" }}
         width={{ base: "full", md: "auto" }}
         alignItems="center"
         flexGrow={1}
@@ -54,18 +78,110 @@ const Header = (props) => {
       </Box>
 
       <ButtonGroup
-        display={{ base: show ? "block" : "none", md: "block" }}
+        display={{ base: showMenu ? "block" : "none", md: "block" }}
         mt={{ base: 10, md: 0 }}
       >
-        <Button bg="transparent" border="1px">
+        <Button
+          bg="transparent"
+          border="1px"
+          onClick={() => {
+            setSignup(true);
+            formOnOpen();
+          }}
+        >
           Create account
         </Button>
-        <Button bg="transparent" border="1px">
+        <Button
+          bg="transparent"
+          border="1px"
+          onClick={() => {
+            setSignup(false);
+            formOnOpen();
+          }}
+        >
           Login
         </Button>
       </ButtonGroup>
+
+      {/* Modal */}
+      <FormModal
+        isOpen={formIsOpen}
+        onClose={formOnClose}
+        isSignup={isSignup}
+      />
     </Flex>
   );
 };
 
 export default Header;
+
+const FormModal = ({ isOpen, onClose, isSignup }) => {
+  const initialRef = React.useRef();
+
+  let passwordRepeatInValid = false;
+
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [password2, setPassword2] = React.useState("");
+
+  if (isSignup) {
+    passwordRepeatInValid = password !== password2;
+  }
+
+  return (
+    <Modal
+      initialFocusRef={initialRef}
+      isOpen={isOpen}
+      onClose={onClose}
+      isCentered
+    >
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>{isSignup ? "Create your account" : "Login"}</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody pb={6}>
+          <FormControl>
+            <FormLabel>E-Mail</FormLabel>
+            <Input
+              ref={initialRef}
+              placeholder="E-Mail"
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </FormControl>
+
+          <FormControl mt={4}>
+            <FormLabel>Password</FormLabel>
+            <Input
+              placeholder="Password"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </FormControl>
+          {isSignup && (
+            <FormControl mt={4}>
+              <FormLabel>Repeat Password</FormLabel>
+              <Input
+                placeholder="Password"
+                type="password"
+                value={password2}
+                isInvalid={passwordRepeatInValid}
+                errorBorderColor="red.300"
+                onChange={(e) => setPassword2(e.target.value)}
+              />
+            </FormControl>
+          )}
+        </ModalBody>
+
+        <ModalFooter>
+          <Button variantColor="blue" mr={3}>
+            Send
+          </Button>
+          <Button onClick={onClose}>Cancel</Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
+};
