c40155d71b06098db726d161176b3905cd75ead0
Oliver Weber <oliver@mondough.io>
2020-03-19 17:11:17 +0100

    App with router and basic style

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+// Styling
+import { ThemeProvider } from "@chakra-ui/core";
+import { theme } from "./utilities/styling";
+import "./base.css";
+
+// Components
+import Header from "./components/Header";
+
+// Views
+import Home from "./views/Home";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Header />
+        <main>
+          {/* A <Switch> looks through its children <Route>s and
+        renders the first one that matches the current URL. */}
+          <Switch>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </main>
+      </Router>
+    </ThemeProvider>
+  );
 }
 
 export default App;
