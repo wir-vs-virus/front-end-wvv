e989164fba0da9ed6e0ddb4dde33aa77417a9464
Oliver Weber <oliver@mondough.io>
2020-03-20 11:17:45 +0100

    Wrap App in AuthContext.Provider

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,14 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import { AuthContextProvider } from "./context/Auth";
+
+ReactDOM.render(
+  <AuthContextProvider>
+    <App />
+  </AuthContextProvider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
